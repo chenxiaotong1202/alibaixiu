9b6b0b4ec07292aaa373295bba68ddbbbb04e0fb
cxt <1160805148@qq.com>
2021-12-31 17:54:06 +0800

    后台侧边栏的滑动

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -15,3 +15,21 @@ $('#logout').on('click',function(){
       })
     }
   })
+
+//左侧边栏 显示与隐藏
+var flag = true
+$('#listBtn').on('click',function(){   
+  if(flag){
+    //内容区置左
+    $('.main .navbar').css('left',0)
+    $('.main').css('padding-left','0px')
+    //左侧显示与隐藏
+    $('.aside').animate({width:'toggle'},150)
+    flag = false
+  }else{
+    $('.main .navbar').css('left','180px')
+    $('.aside').animate({width:'toggle'},150)
+    $('.main').css('padding-left','185px')
+    flag = true
+  }
+})
