f9d7b024eeb0e9b6d56831f664eaaefebaf27e18
cxt <1160805148@qq.com>
2022-01-06 16:18:24 +0800

    完成后台文章的所有操作

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -1,3 +1,17 @@
+//封装函数--用于解析地址栏中传递过来的参数
+function getUrlParams(name){
+    var paramsAry = location.search.substr(1).split('&')  //输出 ['id=61d5031dbc944a08e59161bf', 'age=20']
+    //循环之后,再与传入的参数进行对比
+    for(var i = 0; i < paramsAry.length; i++){
+        var tmp = paramsAry[i].split('=')   //输出 ['id', '61d5031dbc944a08e59161bf']  ['age', '20']
+        if(tmp[0] == name) {
+            return tmp[1]
+        }
+    }
+    //传入的参数与地址栏不一致 返回-1
+    return -1
+}
+
 //查询分类列表
 $.ajax({
     type: 'get',
@@ -15,7 +29,7 @@ $.ajax({
 })
 
 //设置上传的文章封面
-$('#feature').on('change',function(){
+$('#modifyArticleBox').on('change','#feature',function(){
     //获取到用户选中的文件
     var formData = new FormData()
     formData.append('cover',this.files[0]) //第一个参数(cover) 是自定义的名称
@@ -39,7 +53,6 @@ $('#feature').on('change',function(){
 //新增文章
 $('#addArticle').on('submit',function(){
     var formData = $(this).serialize()
-    
     $.ajax({
         type: 'post',
         url: '/posts',
@@ -55,4 +68,53 @@ $('#addArticle').on('submit',function(){
 })
 
 
-//编辑文章
+//渲染修改文章数据
+//获取地址栏中传递过来的参数
+var articleId = getUrlParams('id')
+if(articleId != -1){
+    //管理员当前进行文章修改操作
+
+    //渲染要修改的数据
+    $.ajax({
+        type: 'get',
+        url: '/posts/' + articleId,
+        success: function(data){
+            //查询分类列表
+            $.ajax({
+                type: 'get',
+                url: '/categories',
+                success: function(categories){
+                    //将分类数据追加到data对象中
+                    data.categories = categories
+                    //查询完分类后再渲染页面数据
+                    var html = template('modifyArticleTpl' , data)
+                    $('#modifyArticleBox').html(html)
+                },
+                error: function(err){
+                    alert('获取分类失败')
+                    console.log(err.responseText);
+                }
+            })
+
+        }
+    })
+    //提交修改文章操作
+    $('#modifyArticleBox').on('submit','#addArticle',function(){
+        var formData = $(this).serialize()
+        $.ajax({
+            type: 'put',
+            url: '/posts/' + articleId,
+            data: formData,
+            success: function(data){
+                location.href = '/admin/posts.html'
+            }
+        })
+
+        return false;
+    })
+
+
+}
+
+
+
--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,10 +1,25 @@
-//处理时间的函数  --调用 rTime('2020-06-27T14:20:27.000000Z')
+//处理时间的函数 
 function formateDate(date){
     //将日期字符串 转为 日期对象
-    date = new Date(date)
-    // console.log(date);  Wed Jan 05 2022 10:31:00 GMT+0800 (中国标准时间)
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    date = new Date(date)     // console.log(date);  Wed Jan 05 2022 10:31:00 GMT+0800 (中国标准时间)
+    var yyyy = date.getFullYear();//获取当前年份
+    var MM = date.getMonth() + 1;//因为getMonth()方法获取的是索引值，获取的月份为0-11，所以要＋1
+    var dd = date.getDate();//从 Date 对象返回一个月中的某一天 (1 ~ 31）
+    var hh = date.getHours();//返回 Date 对象的小时 (0 ~ 23)
+    var mm = date.getMinutes();//返回 Date 对象的分钟 (0 ~ 59)
+    MM = checkTime(MM);//调用下面的checkTime函数，设置小于10的时间数字格式，例如5秒显示成05秒
+    dd = checkTime(dd);
+    hh = checkTime(hh);
+    mm = checkTime(mm);
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;//这里如果是一位数则在前面添加一位0
+        }
+        return i;
+     }
+    return yyyy + '-' + MM + '-' + dd
 }
+
 
 //渲染所有文章列表数据
 $.ajax({
@@ -67,6 +82,22 @@ $('#filterFrom').on('submit',function(){
             $('#pageBox').html(html)
         }
     })
-    
     return false
+})
+
+//删除文章
+$('#postsBox').on('click', '.del',function(){
+    var id = $(this).attr('data-id')
+    if(confirm('您确定要删除这篇文章吗?')){
+        $.ajax({
+            type: 'delete',
+            url: '/posts/' + id,
+            success: function(){
+                location.reload()
+            },
+            error: function(err){
+                console.log(err)
+            }
+        })
+    }
 })
